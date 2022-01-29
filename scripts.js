cb467d986437b8691f068d228ad0b007afb3d3bd
jezstdio <jezstdio@protonmail.com>
2022-01-29 09:05:39 +0100

    show character count

--- scripts.js
+++ scripts.js
@@ -2,12 +2,24 @@ const button = document.getElementById("button");
 const sum = document.getElementById("sum");
 const workerName = document.getElementById("name");
 const workerPosition = document.getElementById("position");
+const character_count = document.getElementById("character_count");
 
 function setName() {
-    sum.innerHTML = `${workerName.value} - ${workerPosition.value}`;
+    sum.innerHTML = `${workerName.value} · ${workerPosition.value}`;
+}
+
+function setCharacterCount() {
+    character_count.innerHTML = workerName.value.length + workerPosition.value.length;
 }
 
 button.onclick = () => window.print();
 
-workerName.onkeyup = setName;
-workerPosition.onkeyup = setName;
+workerName.onkeyup = () => {
+    setName();
+    setCharacterCount();
+};
+
+workerPosition.onkeyup = () => {
+    setName();
+    setCharacterCount();
+};
